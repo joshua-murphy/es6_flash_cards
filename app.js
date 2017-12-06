b2621fed2268a1064b04b0fff7801a5c0445fc20
joshua-murphy <josh.murphy11@gmail.com>
2017-12-06 09:22:41 -0700

    cleaned up unnessary js

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 $(document).ready( function() {
-  $(".card-body").attr({ "data-toggle": "popover", "data-placement": "bottom", 
-    "data-title": "Answer:" }).popover().click( () => $(this).popover()) 
-})
+  $(".card-body").attr({ "data-toggle": "popover", "data-placement": "bottom", "data-title": "Answer:" }).popover() })
